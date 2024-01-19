d31b222cb7073cfb929f7344fe4152f5b5ebd935
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-01-19 18:01:25 +0300

    site text changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FindSearch from '../components/Find & Search'
+import Booking from '../components/Booking'
 
 function Home() {
   return (
     <>
     <FindSearch />
+    <Booking />
     </>
   )
 }
---
+++ src/components/Booking.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Booking = () => {
+    return (
+        <>
+            <section>
+                <div className='flex flex-col'>
+                    <div className='text-center mx-0 my-auto'>
+                        <h3 className='text-lg font-medium'>Book your space now</h3>
+                        <h2 className='text-4xl font-extrabold mx-0 mt-5 mb-12'>Affordable and Spacious Work Spaces</h2>
+                    </div>
+                </div>
+
+            </section>
+        </>
+    )
+}
+
+export default Booking
