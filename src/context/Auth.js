0aeee377b04e45ceeed73cfaa39ec8b7bab75026
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-04-16 16:18:39 +0300

    fix: Fixed signup issue

--- src/context/Auth.js
+++ src/context/Auth.js
@@ -1,36 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, onAuthStateChanged, signOut} from "firebase/auth";
+import React  from 'react'
+import { GoogleAuthProvider, signInWithRedirect, onAuthStateChanged, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../firebase';
 import GoogleButton from 'react-google-button';
 
+export const Auth = () => {
+    const doCreateUserWithEmailAndPassword = async (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
-const Auth = () => {
 
-    const [user, setUser] = useState({});
+    const handleGoogleClick = async () => {
+        try {
+            const provider = new GoogleAuthProvider();
+            const result = await signInWithRedirect(auth, provider);
+            //result.user;
+            return result
 
-    const handleGoogleClick = async() => {
-        try{
-        const provider = new GoogleAuthProvider();
-        const result = await signInWithRedirect(auth, provider);
-        const user = result.user;
-        
         } catch (error) {
             console.log(error);
         }
-
     }
+
 
     const logOut = () => {
-        signOut(auth)
+        return auth.signOut();
     }
 
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            setUser(user);
-            console.log("User", user);
-        });
-        
-    }, [])
+
+
     return (
         <div className='flex items-center justify-center mt-5'>
             <button onClick={handleGoogleClick} type='button' title='Continue with Google' >
@@ -40,4 +37,4 @@ const Auth = () => {
     )
 }
 
-export default Auth
+
